f03e8bccbaf8c9d5cfef864d41bc964c4bcbed5f
talia <al427849@uji.es>
2026-04-21 21:12:15 +0200

    ListOfProposedCandidates model corregido.

--- src/main/java/es/uji/ei1027/proyectoOvi/models/ListOfProposedCandidates.java
+++ src/main/java/es/uji/ei1027/proyectoOvi/models/ListOfProposedCandidates.java
@@ -3,33 +3,29 @@ package es.uji.ei1027.proyectoOvi.models;
 import java.util.Date;
 
 public class ListOfProposedCandidates {
-    private String request_Id;
-    private String pap_patiID;
-    private float sutainabilityScore;
+    private String list_id;
+    private float suitabilityScore;
     private Date proposalDate;
+    private String pappati_id;
+    private String request_id;
 
-    public String getRequest_Id() {
-        return request_Id;
+    public ListOfProposedCandidates() {
     }
 
-    public void setRequest_Id(String request_Id) {
-        this.request_Id = request_Id;
+    public String getList_id() {
+        return list_id;
     }
 
-    public String getPap_patiID() {
-        return pap_patiID;
+    public void setList_id(String list_id) {
+        this.list_id = list_id;
     }
 
-    public void setPap_patiID(String pap_patiID) {
-        this.pap_patiID = pap_patiID;
+    public float getSuitabilityScore() {
+        return suitabilityScore;
     }
 
-    public float getSutainabilityScore() {
-        return sutainabilityScore;
-    }
-
-    public void setSutainabilityScore(float sutainabilityScore) {
-        this.sutainabilityScore = sutainabilityScore;
+    public void setSuitabilityScore(float suitabilityScore) {
+        this.suitabilityScore = suitabilityScore;
     }
 
     public Date getProposalDate() {
@@ -40,13 +36,30 @@ public class ListOfProposedCandidates {
         this.proposalDate = proposalDate;
     }
 
+    public String getPappati_id() {
+        return pappati_id;
+    }
+
+    public void setPappati_id(String pappati_id) {
+        this.pappati_id = pappati_id;
+    }
+
+    public String getRequest_id() {
+        return request_id;
+    }
+
+    public void setRequest_id(String request_id) {
+        this.request_id = request_id;
+    }
+
     @Override
     public String toString() {
         return "ListOfProposedCandidates{" +
-                "request_Id='" + request_Id + '\'' +
-                ", pap_patiID='" + pap_patiID + '\'' +
-                ", sutainabilityScore=" + sutainabilityScore +
+                "list_id='" + list_id + '\'' +
+                ", suitabilityScore=" + suitabilityScore +
                 ", proposalDate=" + proposalDate +
+                ", pappati_id='" + pappati_id + '\'' +
+                ", request_id='" + request_id + '\'' +
                 '}';
     }
 }
